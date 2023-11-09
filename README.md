cafabd60d4bfa728d9b541e8e1adf5abc6d53706
Juraj Michálek <juraj.michalek@espressif.com>
2023-11-09 09:08:13 +0100

    add author filter

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Report CSV to HTML converter
 
-Converter which reads CSV with report and converts it to HTML.
+Converter which reads CSV with report and converts it to HTML. Supports following content filters:
+- `--date`
+- `--author`
 
 ## Usage
 
 
 ```
-cargo run -- input.csv "date-to-filter" >output.html
+cargo run -- input.csv --date "date-to-filter" >output.html
 ```
 
 Example:
 
 ```
-cargo run -- input.csv "9/10/2023" >out.html
+cargo run -- input.csv --date "9/10/2023" >out.html
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use clap::Parser;
 use csv::Reader;
 use std::collections::HashMap;
-use std::env;
 use std::error::Error;
 use urlencoding::decode;
+
+#[derive(Parser)]
+#[command(name = "CSV Report Processor")]
+#[command(about = "Processes a CSV file to produce a filtered work report", long_about = None)]
+struct Cli {
+    /// Sets the input CSV file to use
+    #[arg(value_parser)]
+    csv_file: String,
+
+    /// Filters records by a specific date
+    #[arg(long, value_name = "DATE")]
+    date: Option<String>,
+
+    /// Filters records by a specific author
+    #[arg(long, value_name = "AUTHOR")]
+    author: Option<String>,
+}
 
 struct Record {
     project: String,
@@ -12,17 +29,60 @@ struct Record {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Get the filename from the command line
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("Usage: {} <csv_file> <date_filter>", args[0]);
-        return Ok(());
-    }
-    let file_name = &args[1];
-    let date_filter = &args[2];
+    let cli = Cli::parse();
 
-    // Create a new CSV reader from the given file
-    let mut reader = Reader::from_path(file_name)?;
+    let mut reader = Reader::from_path(cli.csv_file)?;
+
+    let mut grouped_records: HashMap<String, Vec<Record>> = HashMap::new();
+
+    for result in reader.records() {
+        let record = result?;
+        let project = record.get(0).unwrap_or_default();
+        let author = record.get(1).unwrap_or_default();
+        let date = record.get(2).unwrap_or_default();
+        let report_encoded = record.get(3).unwrap_or_default();
+
+        // Apply date filter if set
+        if let Some(ref filter) = cli.date {
+            if date != filter {
+                continue;
+            }
+        }
+
+        // Apply author filter if set
+        if let Some(ref filter) = cli.author {
+            if author != filter {
+                continue;
+            }
+        }
+
+        // URL-decode the report
+        let report = decode(report_encoded).expect("UTF-8").to_string();
+
+        let r = Record {
+            project: project.to_string(),
+            author: author.to_string(),
+            date: date.to_string(),
+            report,
+        };
+
+        grouped_records
+            .entry(project.to_string())
+            .or_insert_with(Vec::new)
+            .push(r);
+    }
+
+    // Sort the project names, ensuring that "Rust" is first
+    let mut project_names: Vec<String> = grouped_records.keys().cloned().collect();
+    project_names.sort_by(|a, b| {
+        if a == "Rust" {
+            std::cmp::Ordering::Less
+        } else if b == "Rust" {
+            std::cmp::Ordering::Greater
+        } else {
+            a.cmp(b)
+        }
+    });
 
     // Start the HTML5 document
     let mut html_output = String::from("<!DOCTYPE html>\n");
@@ -37,57 +97,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     html_output.push_str("<table border='1'>\n");
     html_output.push_str("<tr><th>Project</th><th>Author</th><th>Date</th><th>Report</th></tr>\n");
 
-    let mut grouped_records: HashMap<String, Vec<Record>> = HashMap::new();
-
-    for result in reader.records() {
-            let record = result?;
-            let project: &str = &record[0];
-            let author: &str = &record[1];
-            let date: &str = &record[2];
-            let report_encoded: &str = &record[3];
-
-            // URL-decode the report
-            let report = decode(report_encoded).expect("UTF-8").to_string();
-
-            let r = Record {
-                project: project.to_string(),
-                author: author.to_string(),
-                date: date.to_string(),
-                report,
-            };
-
-            if date != date_filter {
-                continue;
-            }
-
-            grouped_records
-                .entry(project.to_string())
-                .or_insert_with(Vec::new)
-                .push(r);
-        }
-
-        // Sort the project names, ensuring that "Rust" is first
-        let mut project_names: Vec<String> = grouped_records.keys().cloned().collect();
-        project_names.sort_by(|a, b| {
-            if a == "Rust" {
-                std::cmp::Ordering::Less
-            } else if b == "Rust" {
-                std::cmp::Ordering::Greater
-            } else {
-                a.cmp(b)
-            }
-        });
-
     // Generate the HTML table using the sorted and grouped records
     for project_name in project_names {
-        let records = grouped_records.get(&project_name).unwrap();
-        for record in records {
-            html_output.push_str("<tr>");
-            html_output.push_str(&format!("<td>{}</td>", record.author));
-            html_output.push_str(&format!("<td>{}</td>", record.project));
-            html_output.push_str(&format!("<td>{}</td>", record.date));
-            html_output.push_str(&format!("<td>{}</td>", record.report));
-            html_output.push_str("</tr>\n");
+        if let Some(records) = grouped_records.get(&project_name) {
+            for record in records {
+                html_output.push_str("<tr>");
+                html_output.push_str(&format!("<td>{}</td>", record.project));
+                html_output.push_str(&format!("<td>{}</td>", record.author));
+                html_output.push_str(&format!("<td>{}</td>", record.date));
+                html_output.push_str(&format!("<td>{}</td>", record.report));
+                html_output.push_str("</tr>\n");
+            }
         }
     }
 
